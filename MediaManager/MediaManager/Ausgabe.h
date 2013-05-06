cc573431da1da74720a5d65cf5620ec9c3c85929
Strahle <manuel-strehle@stud.hs-esslingen.de>
2013-05-06 14:30:34 +0200

    Grundgerüst mit Header und C Dateien

---
+++ MediaManager/MediaManager/Ausgabe.h
@@ -0,0 +1,8 @@
+void showGenere (void);
+//Zeigt eine Liste aller Genere an
+
+void showList (Knoten * Knot);
+//Zeigt eine Liste aller Titel an (auf einer Seite)
+
+void showDetail (char * Pfad , Knoten * List);
+//Zeigt eine Liste mit den Details eines Elementes an
---
+++ MediaManager/MediaManager/Eingabe.h
@@ -0,0 +1,11 @@
+Details * Detail addItemInput (void);
+//Frägt die Werte für das neue Item ab
+//Gibt bei falscher Eingabe Fehlermeldungen zurück
+//Bei Abbruch oder krotischem Fehler wird NULL zurück gegeben
+
+Knoten * Knot deleteItemInput (Knoten * Knot);
+//Frägt ab welches Element gelöscht werden soll
+//Sicherheitsabfrage ob das Element wirklich gelöscht werden soll
+
+Details * Detail searchItem Input (void);
+//Frägt den/die Suchparameter ab
